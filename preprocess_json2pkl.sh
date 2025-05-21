f7d11973c71c0846654664a11c918a5500a32f29
kaixiang <n28124058@gs.ncku.edu.tw>
2025-05-21 17:33:41 +0800

    fix path in script.

--- preprocess_json2pkl.sh
+++ preprocess_json2pkl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python preprocess_code/Gen_Mask_JSON.py --slide_path="/dataset/images/" --json_path="/dataset/annotations/" --save_path="/dataset/"
 python preprocess_code/filtering_to_pkl_FL.py --tifroot="/dataset/images/" --maskroot="/dataset/masks/" --roiroot="/dataset/rois/" --saveroot="/dataset/pkl/"
-python preprocess_code/Gen_datalist.py --pklroot_label="/dataset/pkl/MRCPS_p512_s384_label_level0/" --pklroot_unlabelt="/dataset/pkl/MRCPS_p512_s512_unlabel_level0/" --saveroot="/dataset/"
+python preprocess_code/Gen_datalist.py --pklroot_label="/dataset/pkl/MRCPS_p512_s384_label_level0/" --pklroot_unlabel="/dataset/pkl/MRCPS_p512_s512_unlabel_level0/" --saveroot="/dataset/"
